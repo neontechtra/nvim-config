b5d1bd42a205e1dc94115eaeaea953f5fd2da6bb
gorship <gorship@kali>
2024-08-26 13:52:32 -0700

    first lua setup

--- init.lua
+++ init.lua
@@ -1,1 +1,15 @@
-print("Hello")
+print("hello")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.scrolloff = 999
+
+
