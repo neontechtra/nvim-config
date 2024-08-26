f31c6ce4542c7799d20ea101759bed7359ba20f1
gorship <gorship@kali>
2024-08-26 14:35:33 -0700

    lazy and colors

--- init.lua
+++ init.lua
@@ -17,9 +17,49 @@ vim.opt.tabstop = 4
 -- set shift width to 4 also
 vim.opt.shiftwidth = 4
 
--- sync clipboards
+-- sync clipboards (pressing P :) )
 vim.opt.clipboard = "unnamedplus"
 -- keep cursor in the middle when scrolling
 vim.opt.scrolloff = 999
 
+-- virtual edit to block mode
+-- allows us to use visual block and select non filled cells
+-- eg. list of preambles.. -> ctrl v (block) , c (change), {new value}
+vim.opt.virtualedit = "block"
 
+-- split when previewing changes
+vim.opt.inccommand = "split"
+
+-- ignore case sensitivity for command search 
+vim.opt.ignorecase = true
+
+-- termguicolors
+vim.opt.termguicolors = true
+
+-- Installing lazy plugin manager
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    "rebelot/kanagawa.nvim"
+})
+
+-- end of plugin setup
+
+-- Set colorscheme
+vim.cmd.colorscheme("kanagawa-wave")
