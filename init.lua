67911b32b6cedce51216a6fbe5025d7d0cdfd21b
gorship <gorship@kali>
2024-08-26 13:56:08 -0700

    comments

--- init.lua
+++ init.lua
@@ -1,15 +1,22 @@
 print("hello")
+-- set numbers and make them relative to cursor
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- split below and to the right
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
+-- turn off linewrap
 vim.opt.wrap = false
 
+-- tab four spaces
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 
+-- sync clipboards
 vim.opt.clipboard = "unnamedplus"
+-- keep cursor in the middle when scrolling
 vim.opt.scrolloff = 999
 
 
